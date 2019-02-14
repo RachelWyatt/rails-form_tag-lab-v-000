71aa5f45be7ca1e56eb0f94c40afff356c7458be
Rachel Wyatt <rw126@evansville.edu>
2019-02-14 19:17:37 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
   end
   
   def new
-    @student = Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
 end
